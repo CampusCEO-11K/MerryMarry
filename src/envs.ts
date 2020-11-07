0d60291e630d00d20da326cd568318563807292d
Noverish <embrapers263@gmail.com>
2020-11-07 23:26:24 +0900

    Added github workflow

--- src/envs.ts
+++ src/envs.ts
@@ -1,5 +1,6 @@
 const { protocol, hostname } = window.location;
 
 export const API_SERVER = (process.env.NODE_ENV === 'production')
-  ? 'https://merrymarry.hyunsub.kim'
-  : `${protocol}//${hostname}:8080`
+  ? 'https://api.merrymarry.hyunsub.kim'
+  : `${protocol}//${hostname}:8080`;
+
