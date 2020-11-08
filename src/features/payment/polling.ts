8942019c76a7cf26712cdfa3a98a42e44f7d6877
Noverish <embrapers263@gmail.com>
2020-11-08 12:45:27 +0900

    clearInterval in payment-polling

--- src/features/payment/polling.ts
+++ src/features/payment/polling.ts
@@ -1,35 +1,30 @@
 export function paymentPolling(): Promise<string> {
   return new Promise((resolve, reject) => {
     const timer = setInterval(() => {
-      const status = localStorage.getItem('payment_status');
-
-      if (status) {
-        localStorage.removeItem('payment_status');
-        switch (status) {
-          case 'success': {
-            const pg_token = localStorage.getItem('payment_pg_token');
-            localStorage.removeItem('payment_pg_token');
-            if (pg_token) {
-              resolve(pg_token);
-            } else {
-              reject('no_token');
-            }
-            return;
-          }
-          case 'failure': {
-            reject('failure');
-            return;
-          }
-          case 'cancel': {
-            reject('cancel');
-            return;
-          }
-          default: {
-            return;
-          }
-        }
-        clearInterval(timer);
+      const status = localStorage.getItem('payment_status') as paymentPolling.Status | null;
+      if (!status) {
+        return;
       }
+
+      switch (status) {
+        case 'success': {
+          const pg_token = localStorage.getItem('payment_pg_token');
+          if (pg_token) {
+            resolve(pg_token);
+          } else {
+            reject('no_token');
+          }
+          break;
+        }
+        default: {
+          reject(status);
+          break;
+        }
+      }
+
+      localStorage.removeItem('payment_status');
+      localStorage.removeItem('payment_pg_token');
+      clearInterval(timer);
     }, 1000);
   })
 }
@@ -37,5 +32,5 @@ export function paymentPolling(): Promise<string> {
 export declare namespace paymentPolling {
   export type Result = string;
 
-  export type Error = 'no_token' | 'failure' | 'cancel';
+  export type Status = 'success' | 'failure' | 'cancel' | 'no_token';
 }
